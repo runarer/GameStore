1e1e77bd4b1d20a1944e6e0b27a6593f409ade6d
runarer <95697611+runarer@users.noreply.github.com>
2026-01-07 10:02:19 +0100

    Games records

--- GameStoreApi/Program.cs
+++ GameStoreApi/Program.cs
@@ -1,6 +1,15 @@
+using GameStoreApi.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new(1, "Street Fighter II", "Fighting", 19.99M,new DateOnly(1992,7,15)),
+    new(2, "Final Fantasy VII Rebirth", "RPG", 69.99M,new DateOnly(2024,2,29)),
+    new(3, "Astro Bot", "Platformer", 59.99M,new DateOnly(2024,9,6)),
+];
+
+// GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
