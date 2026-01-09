d5b330ac2bc48abd232414e68a97fdc0c0a552d0
runarer <95697611+runarer@users.noreply.github.com>
2026-01-09 12:56:49 +0100

    Seeds the DB

--- GameStoreApi/Program.cs
+++ GameStoreApi/Program.cs
@@ -1,12 +1,30 @@
 using GameStoreApi.Data;
 using GameStoreApi.Endpoints;
+using GameStoreApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
 var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.Services.AddSqlite<GameStoreContext>(
+    connString,
+    optionsAction: options => options.UseSeeding((context, _) =>
+    {
+        if (!context.Set<Genre>().Any())
+        {
+            context.Set<Genre>().AddRange(
+                new Genre { Name = "Fighting"},
+                new Genre { Name = "RPG"},
+                new Genre { Name = "Platformer"},
+                new Genre { Name = "Racing"},
+                new Genre { Name = "Sports"}
+            );
+
+            context.SaveChanges();
+        }
+    })
+);
 
 var app = builder.Build();
 
